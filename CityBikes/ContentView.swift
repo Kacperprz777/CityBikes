90a74a8a07182c7c2e874fb2fa8ce985e961dd6f
Kacper Przybyła <kacperprzybyla@Air-Kacper.Dlink>
2024-04-14 21:56:27 +0200

    add requestLocationView

--- CityBikes/ContentView.swift
+++ CityBikes/ContentView.swift
@@ -8,14 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var locationManager = LocationManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+        BikeStationsScreen()
+            .environmentObject(locationManager)
     }
 }
 
