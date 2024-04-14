f10c0315b7a1c9ae44d789782c16def0cd21a96f
Kacper Przybyła <kacperprzybyla@Air-Kacper.Dlink>
2024-04-14 21:06:31 +0200

    initial commit

---
+++ CityBikes/CityBikesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityBikesApp.swift
+//  CityBikes
+//
+//  Created by Kacper Przybyła on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CityBikesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
