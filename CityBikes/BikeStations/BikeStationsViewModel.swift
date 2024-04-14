37e6be8d0ed412cd1a015aea4aa90b70a9902732
Kacper Przybyła <kacperprzybyla@Air-Kacper.Dlink>
2024-04-14 21:53:29 +0200

    add bikeStation screen and vm

---
+++ CityBikes/BikeStations/BikeStationsViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  BikeStationsViewModel.swift
+//  CityBikes
+//
+//  Created by Kacper Przybyła on 14/04/2024.
+//
+
+import Foundation
+import Combine
+
+final class BikeStationsViewModel: ObservableObject {
+    
+    @Published var allStations: [Station] = []
+    
+    private let dataService = StationsService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        addSubscribers()
+    }
+    
+    func addSubscribers() {
+        dataService.$allStations
+            .sink { [weak self] returnedStations in
+                self?.allStations = returnedStations
+            }
+            .store(in: &cancellables)
+    }
+    
+}
---
+++ CityBikes/BikeStations/views/BikeStationsScreen.swift
@@ -0,0 +1,27 @@
+//
+//  BikeStationsScreen.swift
+//  CityBikes
+//
+//  Created by Kacper Przybyła on 14/04/2024.
+//
+
+import SwiftUI
+
+struct BikeStationsScreen: View {
+    
+    @EnvironmentObject var locationManager: LocationManager
+    @StateObject var viewModel: BikeStationsViewModel = BikeStationsViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            if locationManager.locationIsDisabled {
+                RequestLocationView()
+            } else {
+                BikeStationsListView(stations:  $viewModel.allStations)
+                    .navigationTitle("City bikes")
+                    .navigationBarTitleDisplayMode(.inline)
+            }
+        }
+    }
+}
+//#Preview {}
