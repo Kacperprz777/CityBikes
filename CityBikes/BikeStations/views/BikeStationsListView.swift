0383325c452df83e1a5b2dea6fc2f68af90277ea
Kacper Przybyła <kacperprzybyla@Air-Kacper.Dlink>
2024-04-14 22:01:24 +0200

    add BikeStationsListView

---
+++ CityBikes/BikeStations/views/BikeStationsListView.swift
@@ -0,0 +1,30 @@
+//
+//  BikeStationsListView.swift
+//  CityBikes
+//
+//  Created by Kacper Przybyła on 14/04/2024.
+//
+
+import SwiftUI
+
+struct BikeStationsListView: View {
+    
+    @Binding var stations: [Station]
+    
+    var body: some View {
+        
+        List {
+            ForEach(stations) { station in
+                NavigationLink(destination: DetailView(viewModel: DetailViewViewModel(station: station))) {
+                    BikeStationRow(station: station)
+                }
+                .listRowSeparator(.hidden)
+                .listRowBackground(Color(UIColor.systemBackground))
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+//#Preview {
+//}
